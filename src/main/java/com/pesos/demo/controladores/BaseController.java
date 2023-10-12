6ca54d3c5236fb35db96d9c0467f1d761f40e6f2
ivanpalacio <palacioivan53@gmail.com>
2023-10-11 21:55:27 -0300

    add query

--- src/main/java/com/pesos/demo/controladores/BaseController.java
+++ src/main/java/com/pesos/demo/controladores/BaseController.java
@@ -1,6 +1,7 @@
 package com.pesos.demo.controladores;
 
 import com.pesos.demo.entidades.Base;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import java.io.Serializable;
 public interface BaseController <E extends Base, Id extends Serializable> {
 
     public ResponseEntity<?> getAll();
+    public ResponseEntity<?> getAll(Pageable pageable);
     public ResponseEntity<?> getOne(@PathVariable Id id);
     public ResponseEntity<?> save(@RequestBody E entity);
     public ResponseEntity<?> update(@PathVariable Id id,@RequestBody E entity);
